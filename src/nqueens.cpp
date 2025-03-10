02b865d3eaada67aaf120f19f03df42f3944f7f4
tuongpsa <156788187+tuongpsa@users.noreply.github.com>
2025-03-10 14:47:14 +0700

    .

--- src/nqueens.cpp
+++ src/nqueens.cpp
@@ -36,6 +36,10 @@ void Try(int row) {
 int main() {
     cout << "Enter number of queens: ";
     cin >> n;
+    if(n<0){
+        cout<<"invalid!";
+        return 0;
+    }
     board.resize(n);
     check.resize(n+1,0);
     check1.resize(2*n,0);
